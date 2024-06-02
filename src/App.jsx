991e1fde1ce23f1bc7963c4b532837311a426be2
its-wasp <its_wasp@outlook.com>
2024-06-02 21:15:06 +0530

    feat: added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Navbar from "./Components/Navbar/Navbar.jsx";
-import Hero from "./Components/Hero/Hero.jsx";
-import About from "./Components/About/About.jsx";
+import Projects from "./Components/HomePage/Projects/Projects.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
+import AboutMe from "./Components/AboutMe/Hero/AboutMeHero.jsx";
+import HomePage from "./Components/HomePage/HomePage.jsx";
 
 const App =() => {
     return (
-        <div>
+        <BrowserRouter>
             <Navbar/>
-            <Hero/>
-            <About/>
-        </div>
+            <Routes>
+                <Route path="/" element={<HomePage />}/>
+                <Route path="/projects" element={<Projects/>}/>
+                <Route path="/about" element={<AboutMe/>}/>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
     );
 }
 export default App;
