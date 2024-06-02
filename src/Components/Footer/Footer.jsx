64291a98d8bc9b4d3390fbd5a1817491e0f3dfc6
its-wasp <its_wasp@outlook.com>
2024-06-02 21:16:40 +0530

    feat: added footer section

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className='app-footer'>
+            <p>© 2024 its-wasp. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
