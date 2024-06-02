b5d08c3fc0699414971c1f5eb0a3a123db4efd6a
its-wasp <its_wasp@outlook.com>
2024-06-02 21:16:22 +0530

    feat: added about me section

---
+++ src/Components/AboutMe/Hero/AboutMeHero.jsx
@@ -0,0 +1,13 @@
+
+import './AboutMeHero.css';
+
+const AboutMe = () => {
+    return (
+        <div className='about-me'>
+            <h1>About Me</h1>
+            <p>Hello, I'm its-wasp. I'm a software developer with a passion for learning and building applications. I have experience in JavaScript, React, and other technologies.</p>
+        </div>
+    );
+}
+
+export default AboutMe;
