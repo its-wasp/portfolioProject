d1ec4c272b7900fd6232c03c942c095c98bf4f68
its-wasp <its_wasp@outlook.com>
2024-06-15 23:00:50 +0530

    update : updated the profile image and logo

--- src/Components/HomePage/Hero/Hero.jsx
+++ src/Components/HomePage/Hero/Hero.jsx
@@ -1,21 +1,28 @@
-import './Hero.css'
-import profile_img from '../../../assets/profile_img.svg'
+import "./Hero.css";
+import profile_img from "../../../assets/profile_img.png";
 import Typewriter from "../../typewriter/Typewriter.jsx";
-const Hero =() => {
-    return(
-        <div className = 'hero'>
-            <img src={profile_img} alt="profile" />
-            <h1><span>I am Suraj, </span>
-                <Typewriter text={["not a frontend developer", "competitive programmer", "AI enthusiast"]} /></h1>
+const Hero = () => {
+  return (
+    <div className="hero">
+      <img src={profile_img} alt="profile" />
+      <h1>
+        <span>I am Suraj, </span>
+        <Typewriter
+          text={[
+            "a frontend developer",
+            "competitive programmer",
+            "AI enthusiast",
+          ]}
+        />
+      </h1>
 
-            {/*<p>I am a frontend developer. I am a frontend developer. I am a frontend developer</p>*/}
-            <div className = "hero-action">
-                <div className = "hero-connect">Connect with me</div>
-                <div className = "hero-resume">My resume</div>
-
-            </div>
-        </div>
-    )
-}
+      {/*<p>I am a frontend developer. I am a frontend developer. I am a frontend developer</p>*/}
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,24 @@
-import './Navbar.css';
-import logo from '../../assets/logo.svg'
-import {Link} from "react-router-dom";
+import "./Navbar.css";
+import logo from "../../assets/top_logo.png";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-    return (
-        <nav className = 'navbar'>
-            <img src={logo} alt = "" />
-            <ul className="nav-menu">
-                <Link to="/"><li>Home</li></Link>
-                <Link to="/about"><li>About Me</li></Link>
-                <Link to="/projects"><li>Projects</li></Link>
-            </ul>
-            <div className="nav-connect">Connect With me</div>
-
-        </nav>
-    );
-}
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="" height="120" width="120" />
+      <ul className="nav-menu">
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/about">
+          <li>About Me</li>
+        </Link>
+        <Link to="/projects">
+          <li>Projects</li>
+        </Link>
+      </ul>
+      <div className="nav-connect">Connect With me</div>
+    </nav>
+  );
+};
 
 export default Navbar;
